122de4d62185bb11eb06e6de89ba8248c3dd1b64
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-03 08:37:34 -0400

    Updated py2app.sh to copy Habitus.app to ~/Applications folder

--- py2app.sh
+++ py2app.sh
@@ -3,3 +3,4 @@ rm -rf build dist
 python setup.py py2app --iconfile images/icon.icns
 mkdir dist/Habitus.app/Contents/Resources/images
 cp images/icon-bw.png dist/Habitus.app/Contents/Resources/images/icon-bw.png
+mv dist/Habitus.app ~/Applications
