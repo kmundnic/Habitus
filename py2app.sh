682a04c7517db7ecde9e4ac43d88621b64ae42d5
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-08 10:20:12 -0400

    Fixed the file to prevent error at launch of Habitus.app

--- py2app.sh
+++ py2app.sh
@@ -1,6 +1,7 @@
-py2applet --make-setup Habitus.py
 rm -rf build dist
+rm setup.py
+py2applet --make-setup Habitus.py
 python setup.py py2app --iconfile images/icon.icns
 mkdir dist/Habitus.app/Contents/Resources/images
 cp images/icon-bw.png dist/Habitus.app/Contents/Resources/images/icon-bw.png
-mv dist/Habitus.app ~/Applications
+mv ./dist/Habitus.app /Applications
