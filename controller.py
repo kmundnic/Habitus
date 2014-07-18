21c84548f6e9687d99bf8519bd1586425c1443d7
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-18 19:29:26 -0400

    New controller.py file with timer controls

--- controller.py
+++ controller.py
@@ -1,18 +1,28 @@
 import rumps
 from data_retriever import DataRetriever
 from data_sender import DataSender
-import controller2
 
 
-class Habitus(rumps.App):
-    def __init__(self):
-        super(Habitus, self).__init__("Habitus", icon="images/icon-bw.png")
-        self.menu = ['Start', 'Stop', None]
+data_retriever = DataRetriever()
+data_sender = DataSender()
 
-    @rumps.clicked('Start')
-    def start_data_collection_timer(self):
-        controller2.retrieve_data_timer.start()
 
-    @rumps.clicked('Stop')
-    def stop_data_collection_timer(self):
-        controller2.retrieve_data_timer.stop()
+def retrieve_data_callback(_):
+    print data_retriever.retrieve_active_app_name()
+
+
+@rumps.timer(10)  # Data is sent every 15min
+def send_data_callback(_):
+    if retrieve_data_timer.is_alive():
+        retrieve_data_timer.stop()
+        data_to_send = list(data_retriever.data)
+        data_retriever.data = []
+        retrieve_data_timer.start()
+
+        if data_to_send:
+            data_sender.write_data(data_to_send)
+            print "Sending data..."
+            data_sender.send_data()
+
+retrieve_data_timer = rumps.Timer(retrieve_data_callback,
+                                  interval=1)  # 1[s] interval
