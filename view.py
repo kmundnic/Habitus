055f20aaa429adaf5233da5f15bee3065fea8342
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-19 11:18:49 -0400

    Added documentation to view.py

--- view.py
+++ view.py
@@ -9,6 +9,14 @@ class Habitus(rumps.App):
         self.menu = ['Start', 'Stop', None]
 
     def set_status(self):
+        """
+        Changes an element in the menu for the user to know if the application
+        is running or is stopped.
+        It is called by the callback functions of the MenuItems 'Start' and
+        'Stop', and checks if the application is running by checking if the
+        data retrieving thread is alive.
+        :return: None
+        """
         if controller.retrieve_data_timer.is_alive():
             try:
                 del self.menu['Stopped']
@@ -25,11 +33,25 @@ class Habitus(rumps.App):
 
     @rumps.clicked('Start')
     def start_data_collection_timer(self, sender):
+        """
+        Callback function for MenuItem 'Start'. self, sender are used as
+        arguments to differentiate between this app (self) and the sender object
+        (MenuItem).
+        :param sender: MenuItem
+        :return: None
+        """
         controller.retrieve_data_timer.start()
         self.set_status()
 
     @rumps.clicked('Stop')
     def stop_data_collection_timer(self, sender):
+        """
+        Callback function for MenuItem 'Stop'. self, sender are used as
+        arguments to differentiate between this app (self) and the sender object
+        (MenuItem).
+        :param sender: MenuItem
+        :return: None
+        """
         controller.retrieve_data_timer.stop()
         self.set_status()
 
