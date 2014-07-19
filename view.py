3643f7720b9aff7824b06c3e4b0f0f1adcafbe78
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-19 11:14:10 -0400

    Created new function in view.py to handle status in menu

--- view.py
+++ view.py
@@ -6,27 +6,30 @@ class Habitus(rumps.App):
 
     def __init__(self):
         super(Habitus, self).__init__("Habitus", icon="images/icon-bw.png")
-        self.menu = ['Start',
-                     'Stop',
-                     # rumps.MenuItem('Stopped', callback=Habitus.state),
-                     None]
+        self.menu = ['Start', 'Stop', None]
 
-    @rumps.clicked('Start')
-    def start_data_collection_timer(self, sender):
-        controller.retrieve_data_timer.start()
+    def set_status(self):
         if controller.retrieve_data_timer.is_alive():
             try:
                 del self.menu['Stopped']
             except KeyError:
                 "Has never stopped yet"
             self.menu.insert_after('Stop', 'Running...')
-
-    @rumps.clicked('Stop')
-    def stop_data_collection_timer(self, sender):
-        controller.retrieve_data_timer.stop()
-        if not controller.retrieve_data_timer.is_alive():
+        else:
             try:
                 del self.menu['Running...']
                 self.menu.insert_after('Stop', 'Stopped')
             except KeyError:
                 print "Already stopped"
+
+
+    @rumps.clicked('Start')
+    def start_data_collection_timer(self, sender):
+        controller.retrieve_data_timer.start()
+        self.set_status()
+
+    @rumps.clicked('Stop')
+    def stop_data_collection_timer(self, sender):
+        controller.retrieve_data_timer.stop()
+        self.set_status()
+
