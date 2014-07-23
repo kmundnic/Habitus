21a67beea694cdd3a67c9ff729c290a552a729f1
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-23 10:19:44 -0400

    Changed py2app.sh to automatically remove /Applications/Habitus.app and copy new Habitus.app to this /Applications

--- py2app.sh
+++ py2app.sh
@@ -7,4 +7,5 @@ py2applet --make-setup Habitus.py
 python setup.py py2app --iconfile images/icon.icns
 mkdir dist/Habitus.app/Contents/Resources/images
 cp images/icon-bw.png dist/Habitus.app/Contents/Resources/images/icon-bw.png
+rm -rf /Applications/Habitus.app
 mv ./dist/Habitus.app /Applications
