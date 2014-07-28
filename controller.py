94cf0cbbb6d328a2bb0238c5d6d85bd06f5091c2
Karel Mundnich <kmundnic@ing.uchile.cl>
2014-07-28 15:10:59 -0400

    TODO: Erase files that have been sent

--- controller.py
+++ controller.py
@@ -4,6 +4,13 @@ from data_retriever import DataRetriever
 from data_sender import DataSender
 
 # TODO: Is it necessary to change this file into a class? Reference problems...
+#
+# TODO: Erase files that have been sent
+# If the files that have been sent are erased, it is quite easy to keep track
+# of the information that has not been sent, and this can be saved into a list
+# that can be easily put into a file and therefore, read to create a queue.
+#
+# TODO: Keep log with logger
 
 RETRIEVE_DATA_INTERVAL = 1
 SEND_DATA_INTERVAL = 15
